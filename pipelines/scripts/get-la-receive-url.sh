6b31da6c5782981e8d3a11b9184397ca3a21c31b
RussSmi <russell_smith@hotmail.com>
2021-04-29 17:12:14 +0100

    debug url script

--- pipelines/scripts/get-la-receive-url.sh
+++ pipelines/scripts/get-la-receive-url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage ./get-la-receive-url.sh -g resource-group-name -n logic-app-name -w workflow-name
-while getopts g:n:w option
+while getopts g:n:w: option
 do
 case "${option}"
 in
@@ -10,6 +10,9 @@ w) WFLOW=${OPTARG};;
 esac
 done
 
-LAURL=$(az rest -m post --header "Accept=application/json" -u "https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/${RG}/providers/Microsoft.Web/sites/${LANAME}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${WFLOW}/triggers/manual/listCallbackUrl?api-version=2018-11-01")
-echo $LAURL
-echo "##vso[task.setvariable variable=lap-receive-url;]LAURL"
+echo resource-group=$RG
+echo logic-app-name=$LANAME
+echo workflow-name=$WFLOW
+
+LAURL=$(az rest -m post --header "Accept=application/json" -u "https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/${RG}/providers/Microsoft.Web/sites/${LANAME}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${WFLOW}/triggers/manual/listCallbackUrl?api-version=2018-11-01" --query value -o tsv)
+echo logic-app-url=$LAURL
