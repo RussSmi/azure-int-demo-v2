a9dc0e23f450d5a684d35e1b286f9fa556dd05d1
RussSmi <russell_smith@hotmail.com>
2021-04-29 17:47:03 +0100

    More bugs

--- pipelines/scripts/get-la-receive-url.sh
+++ pipelines/scripts/get-la-receive-url.sh
@@ -14,8 +14,6 @@ echo resource-group=$RG
 echo logic-app-name=$LANAME
 echo workflow-name=$WFLOW
 
-CMD="az rest -m post --header \"Accept=application/json\" -u \"https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/${RG}/providers/Microsoft.Web/sites/${LANAME}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${WFLOW}/triggers/manual/listCallbackUrl?api-version=2018-11-01\""
-echo $CMD
 
 LAURL=$(az rest -m post --header "Accept=application/json" -u "https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/${RG}/providers/Microsoft.Web/sites/${LANAME}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${WFLOW}/triggers/manual/listCallbackUrl?api-version=2018-11-01" --query value -o tsv)
 echo logic-app-url=$LAURL
