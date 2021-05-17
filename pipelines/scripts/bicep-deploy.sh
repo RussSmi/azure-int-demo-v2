4fab48caed68166eb701be14898f42fede046f94
Russell Smith <44894904+RussSmi@users.noreply.github.com>
2021-05-17 16:31:08 +0100

    Update bicep-deploy.sh

--- pipelines/scripts/bicep-deploy.sh
+++ pipelines/scripts/bicep-deploy.sh
@@ -22,10 +22,10 @@ az bicep install
 CURRENT_TIME=$(date "+%Y_%m_%d_%H_%M_%S")
 echo "Current Time : $CURRENT_TIME"
 
-DEPLOY_NAME=$CURRENT_TIME
+DEPLOY_NAME=$DEPLOYPREFIX_$CURRENT_TIME
 echo "Deploy name : $DEPLOY_NAME"
 
 echo 'Deploy bicep file...'
-az deployment group create --name $CURRENT_TIME --resource-group $RG --template-file $BICEPFILE --parameters $PARAMFILE
+az deployment group create --name $DEPLOY_NAME --resource-group $RG --template-file $BICEPFILE --parameters $PARAMFILE
 
 echo 'Done.'
